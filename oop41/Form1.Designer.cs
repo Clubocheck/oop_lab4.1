9719450d6382dbb848721358c2c446bff3dfdadb
Lenovo <Lenovo@DESKTOP-QFLM5L3>
2023-05-25 20:06:35 +0500

    Добавьте файлы проекта.

---
+++ oop41/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace oop41
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(12, 15);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(54, 24);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Ctrl";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(12, 45);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(96, 24);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Switch All";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 75);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Del";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            KeyPreview = true;
+            Name = "Form1";
+            Text = "Form1";
+            Paint += Form1_Paint;
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            MouseClick += Form1_MouseClick;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Button button1;
+    }
+}
